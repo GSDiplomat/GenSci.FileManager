bab2b7828541b0bb78a51311190ee5a4b098d861
I.Borisoglebskiy <i.borisoglebskiy@gmail.com>
2020-05-15 11:50:24 +0300

    Drive switching added to Element list view.

--- Source/GenSci.FM.Core/ControlsContexts/ElementListContext.cs
+++ Source/GenSci.FM.Core/ControlsContexts/ElementListContext.cs
@@ -2,6 +2,7 @@
 using GenSci.FM.ElementList;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Input;
@@ -12,9 +13,20 @@ namespace GenSci.FM.Core.Context
     {
         private ElementStruct _selectedElement;
         private GenSciCommand _enterDirectory;
+        private KeyValuePair<DriveInfo, string> _selectedDrive;
 
-        public ElementListModel ElementListModel { get; set; } = new ElementListModel();
+        public ElementListContext()
+        {
+            foreach (var drive in DriveInfo.GetDrives())
+            {
+                DrivePaths.Add(drive, drive.Name);
+            }
 
+            SelectedDrive = DrivePaths.First();
+        }
+
+        public ElementListModel ElementListModel { get; } = new ElementListModel();
+        public Dictionary<DriveInfo, string> DrivePaths { get; } = new Dictionary<DriveInfo, string>();
 
         public ElementStruct SelectedElement
         {
@@ -28,6 +40,26 @@ namespace GenSci.FM.Core.Context
                 }
             }
         }
+
+        public KeyValuePair<DriveInfo, string> SelectedDrive
+        {
+            get => _selectedDrive;
+            set
+            {
+                if (value.Key != _selectedDrive.Key)
+                {
+                    if (_selectedDrive.Key != null)
+                        DrivePaths[_selectedDrive.Key] = ElementListModel.AddressLine;
+
+                    _selectedDrive = value;
+
+                    ElementListModel.AddressLine = DrivePaths[value.Key];
+
+                    OnPropertyChanged();
+                }
+            }
+        }
+
 
         public GenSciCommand EnterDirectory => _enterDirectory ??
             (_enterDirectory = new GenSciCommand(
@@ -44,7 +76,7 @@ namespace GenSci.FM.Core.Context
                     }
                     else if (SelectedElement.ElementType == eElementType.Directory)
                     {
-                        ElementListModel.AddressLine += SelectedElement.FileName;
+                        ElementListModel.AddressLine += SelectedElement.FileName + "\\";
                     }
                 }));
     }
--- Source/GenSci.FM.Core/GenSciContext.cs
+++ Source/GenSci.FM.Core/GenSciContext.cs
@@ -1,11 +1,66 @@
-﻿using System.Collections.Generic;
+﻿using GenSci.FM.Core.Notify;
+using System;
+using System.Collections.Generic;
 using System.Text;
 using System.Windows.Input;
 
 namespace GenSci.FM.Core.Context
 {
-    public class GenSciContext
+    public class GenSciContext : NotifyPropertyBase
     {
-        public List<ElementListContext> ElementListContext { get; set; } = new List<ElementListContext> { new ElementListContext(), new ElementListContext() };
+        private GenSciCommand _swapPanels;
+        private ElementListContext _elementListContextLeft = new ElementListContext();
+        private ElementListContext _elementListContextRight = new ElementListContext();
+        private ElementListContext _activeElementList;
+
+        public ElementListContext ElementListContextLeft
+        {
+            get => _elementListContextLeft;
+            set
+            {
+                if (value != _elementListContextLeft)
+                {
+                    _elementListContextLeft = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ElementListContext ElementListContextRight
+        {
+            get => _elementListContextRight;
+            set
+            {
+                if (value != _elementListContextRight)
+                {
+                    _elementListContextRight = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ElementListContext ActiveElementList
+        {
+            get => _activeElementList;
+            set
+            {
+                if (value != _activeElementList)
+                {
+                    _activeElementList = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public GenSciCommand SwapPanels => _swapPanels ?? (_swapPanels = new GenSciCommand(obj => Swap()));
+
+        private void Swap()
+        {
+            var templContext = ElementListContextLeft;
+
+            ElementListContextLeft = ElementListContextRight;
+
+            ElementListContextRight = templContext;
+        }
     }
 }
